eae59a8042adfb30f05db81398be53ad209947c4
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2022-11-22 18:35:50 -0300

    fix/user/

--- api/src/controllers/users/index.ts
+++ api/src/controllers/users/index.ts
@@ -59,5 +59,5 @@ export const deleteUserController = async(req: Request, res:Response) => {
 export const profileUserController = async(req: Request, res:Response) => {
     const id = req.user.id;
     const user = await profileUserService(id)
-    return res.status(200).json({user})
+    return res.status(200).json(user)
 }
